16644bdfe32e62b0f4caf3163b5ab3b68ff25775
mateovelmontes <oleg.eliiashiv@cloudcod.com>
2024-09-22 17:39:02 +0300

    Migration to postgresql

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI, HTTPException, Request
-from models import User, init_db
+from fastapi import FastAPI, HTTPException, Depends, Query
+from fastapi.responses import PlainTextResponse
+from models import User, UserCreate, UserAuthenticate, Token, RefreshToken
+from models import BillCreate, Bill, BillResponse, init_db
+from typing import Optional, List
 from contextlib import asynccontextmanager
-from models import init_db
+from datetime import datetime
 from pprint import pformat
+import uuid
+import pyqrcode
+from qr2text import QR
+from PIL import Image
 
 
 @asynccontextmanager
@@ -14,59 +21,40 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/register")
-async def register(request: Request):
-    data = await request.json()
-    username = data.get("username")
-    password = data.get("password")
-    full_name = data.get("full_name")
-    if await User.get_user_id(username):
+async def register(user: UserCreate):
+    if await User.get_user_id(user.username):
         raise HTTPException(status_code=400, detail="Username already registered")
-    await User.create(username, password, full_name)
+    await User.create(user)
     return {"message": "User registered successfully"}
 
 
 @app.post("/login")
-async def login(request: Request):
-    data = await request.json()
-    username = data.get("username")
-    password = data.get("password")
-    if not await User.authenticate(username, password):
+async def login(user: UserAuthenticate):
+    if not await User.authenticate(user.username, user.password):
         raise HTTPException(status_code=400, detail="Invalid username or password")
-
-    access_token = User.create_access_token(username=username)
-    refresh_token = User.create_refresh_token(username=username)
-
-    user_id = await User.get_user_id(username)
-
-    await User.save_tokens(
+    access_token = Token.create_access_token(username=user.username)
+    new_refresh_token = Token.create_refresh_token(username=user.username)
+    user_id = await User.get_user_id(user.username)
+    await Token.save_tokens(
         user_id=user_id,
         access_token=access_token,
-        refresh_token=refresh_token
-        )
-
+        refresh_token=new_refresh_token
+    )
     return {
         "access_token": access_token,
-        "refresh_token": refresh_token,
+        "refresh_token": new_refresh_token,
         "token_type": "bearer"
     }
 
 
 @app.post("/refresh-token")
-async def refresh_token(request: Request):
-    try:
-        data = await request.json()
-        refresh_token = data.get("refresh_token")
-        if not refresh_token:
-            raise HTTPException(status_code=400, detail="Refresh token is missing")
-        username = await User.verify_refresh_token(refresh_token)
-        if not username:
-            raise HTTPException(status_code=401, detail="Invalid refresh token")
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-    new_access_token = User.create_access_token(username=username)
+async def refresh_token(token: RefreshToken):
+    username = await Token.verify_refresh_token(token.refresh_token)
+    if not username:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
+    new_access_token = Token.create_access_token(username=username)
     user_id = await User.get_user_id(username)
-    await User.update_access_token(
+    await Token.update_access_token(
         user_id=user_id,
         new_access_token=new_access_token
         )
@@ -74,3 +62,118 @@ async def refresh_token(request: Request):
         "access_token": new_access_token,
         "token_type": "bearer"
     }
+
+
+@app.post("/bill", response_model=BillResponse)
+async def create_bill(receipt_data: BillCreate, user_id: int = Depends(Token.get_current_user)):
+    bill_id = (uuid.uuid4().int >> 64) % 100000
+    bill_id_str = str(bill_id).ljust(5, '0')
+    receipt_entry = {
+        "id": bill_id_str,
+        "products": receipt_data.products,
+        "payment": receipt_data.payment.model_dump(),
+        "total": sum([product.quantity * product.price for product in receipt_data.products]),
+        "created_at": datetime.now().timestamp(),
+        "user_id": user_id,
+        "notes": receipt_data.notes
+    }
+    await Bill.save_bill(receipt_entry)
+    return BillResponse.from_receipt(receipt_entry)
+
+
+@app.get("/bills", response_model=List[BillResponse])
+async def get_bills(
+    user_id: int = Depends(Token.get_current_user),
+    date_from: Optional[str] = None,
+    date_to: Optional[str] = None,
+    min_total: Optional[float] = None,
+    max_total: Optional[float] = None,
+    payment_type: Optional[str] = None,
+    limit: int = 10,
+    offset: int = 0
+):
+    receipts = await Bill.get_filtered_bills(
+        user_id=user_id,
+        date_from=date_from,
+        date_to=date_to,
+        min_total=min_total,
+        max_total=max_total,
+        payment_type=payment_type,
+        limit=limit,
+        offset=offset
+    )
+    return receipts
+
+
+@app.get("/bill/{bill_id}", response_class=PlainTextResponse)
+async def get_bill_text(bill_id: str, line_length: int = 40):
+    receipt = await Bill.get_bill_by_id(bill_id)
+    nameinbill = await Bill.get_user_by_bill_id(bill_id)
+    if not receipt:
+        raise HTTPException(status_code=404, detail="Receipt not found")
+    formatted_receipt = format_bill(receipt, line_length, nameinbill)
+    return formatted_receipt
+
+
+def format_bill(receipt: BillResponse, line_length: int, nameinbill: dict) -> str:
+    if receipt.payment.type == 'nano':
+        receipt_text = image_to_ascii('img/nano-logo.png', line_length)+ "\n"
+        receipt_text += f"{nameinbill['full_name']}/{nameinbill['username']}".center(line_length) + "\n"
+        qr_code = generate_qr_ascii('https://blocklattice.io/block/CCC4996E6CFFB4C0035A7DD49CE95ADBD56CB14AAE10AB71261F8630F1BD6F82', line_length)
+    else:
+        receipt_text = f"{nameinbill['full_name']}/{nameinbill['username']}".center(line_length) + "\n"
+        qr_code = generate_qr_ascii(receipt.url, line_length)
+    receipt_text += "=" * line_length + "\n"
+    for product in receipt.products:
+        product_line = f"{product.quantity:.2f} x {product.price:,.2f}"
+        receipt_text += f"{product_line:<{line_length}}\n"
+        name_length = line_length - len(f"{product.total:,.2f}") - 1
+        receipt_text += f"{product.name[:name_length]:<{name_length}} {product.total:>{len(f'{product.total:,.2f}')},.2f}\n"
+        receipt_text += "-" * line_length + "\n"
+    receipt_text += f"{'СУМА':<15}{receipt.total:>{line_length - 15},.2f}\n"
+    receipt_text += f"{'Оплата':<15}{receipt.payment.amount:>{line_length - 15},.2f}\n"
+    receipt_text += f"{'Решта':<15}{receipt.change:>{line_length - 15},.2f}\n"
+    receipt_text += "=" * line_length + "\n"
+    receipt_text += f"{receipt.created_at:^{line_length}}\n"
+    receipt_text += f"{'Дякуємо за покупку!':^{line_length}}\n"
+    receipt_text += "\n" + qr_code
+    return receipt_text
+
+
+def generate_qr_ascii(qr_code_link: str, line_length: int) -> str:
+    QR.from_text = classmethod(from_text_mock)
+    qr = QR.from_text(qr_code_link)
+    qr_ascii = qr.to_ascii_art(trim=True, pad=0)
+    qr_lines = qr_ascii.splitlines()
+    centered_qr_lines = [
+        line.center(line_length) for line in qr_lines
+    ]
+
+    return '\n'.join(centered_qr_lines)
+
+
+def image_to_ascii(image_path: str, output_width: int = 100) -> str:
+    ASCII_CHARS = "@%#*+=-:. "
+    image = Image.open(image_path)
+    grayscale_image = image.convert("L")
+    original_width, original_height = grayscale_image.size
+    aspect_ratio = original_height / original_width
+    output_height = int(aspect_ratio * output_width * 0.55)
+    resized_image = grayscale_image.resize((output_width, output_height))
+    pixels = list(resized_image.getdata())
+    scale_factor = (255 // len(ASCII_CHARS)) + 1
+    ascii_art = ""
+    for y in range(output_height):
+        line = "".join(ASCII_CHARS[pixel // scale_factor] for pixel in pixels[y * output_width:(y + 1) * output_width])
+        ascii_art += line + "\n"
+    return ascii_art
+
+
+def from_text_mock(cls, text: str) -> 'QR':
+    code = pyqrcode.create(text, encoding='UTF-8', error='L')
+    data = code.text().splitlines()
+    qr = cls(len(data))
+    qr.canvas.pixels = [
+        [int(px) for px in row] for row in data
+    ]
+    return qr
--- models.py
+++ models.py
@@ -1,16 +1,19 @@
 import aiosqlite
+from pydantic import BaseModel, Field, condecimal, constr
+from typing import List, Optional, Literal, Dict, Any
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
-from jose import jwt
+from jose import jwt, JWTError, ExpiredSignatureError
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
+
 
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
-
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 DB_PATH = "database.db"
-
-
 SECRET_KEY = "DATAENCRYPTIONSTANDARDISABESTSYMMETRIC-KEYALGORITHMFORTHEENCRYPTION"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60*24
 REFRESH_TOKEN_EXPIRE_DAYS = 30
 
 
@@ -31,20 +34,103 @@ async def init_db():
                             refresh_expires_at TEXT NOT NULL,
                             FOREIGN KEY (user_id) REFERENCES users (id))''')
 
+        await db.execute('''CREATE TABLE IF NOT EXISTS bills (
+                            id TEXT PRIMARY KEY,
+                            user_id INTEGER NOT NULL,
+                            created_at REAL NOT NULL,
+                            total REAL NOT NULL,
+                            payment_type TEXT NOT NULL,
+                            payment_amount REAL NOT NULL,
+                            notes TEXT,
+                            FOREIGN KEY (user_id) REFERENCES users (id))''')
+
+        await db.execute('''CREATE TABLE IF NOT EXISTS bill_products (
+                            bill_id TEXT,
+                            name TEXT NOT NULL,
+                            price REAL NOT NULL,
+                            quantity REAL NOT NULL,
+                            FOREIGN KEY (bill_id) REFERENCES bills (id))''')
         await db.commit()
+
+
+class UserCreate(BaseModel):
+    username: str = Field(..., min_length=3, max_length=50)
+    password: str = Field(..., min_length=8)
+    full_name: str = Field(..., min_length=3, max_length=100)
+
+
+class UserAuthenticate(BaseModel):
+    username: str
+    password: str
+
+
+class RefreshToken(BaseModel):
+    refresh_token: str
+
+
+class Product(BaseModel):
+    name: str = Field(..., min_length=1)
+    price: Optional[condecimal(gt=0)]  # Вартість повинна бути більше 0
+    quantity: condecimal(gt=0)  # Кількість/вага повинна бути більше 0
+
+
+class ProductResponse(BaseModel):
+    name: str
+    price: float
+    quantity: float
+    total: float
+
+
+class Payment(BaseModel):
+    type: Literal["cash", "card", "nano"]  # Тип повинен бути "cash" або "card"
+    amount: condecimal(gt=0)  # Сума повинна бути більше 0
+
+
+class BillCreate(BaseModel):
+    products: List[Product]
+    payment: Payment
+    notes: Optional[str] = None
+
+
+class BillResponse(BaseModel):
+    id: str
+    products: List[ProductResponse]
+    payment: Payment
+    total: float
+    change: float
+    created_at: str
+    notes: Optional[str] = None
+    url: str
+
+    @classmethod
+    def from_receipt(cls, receipt_entry: Dict[str, Any]):
+        receipt_entry["change"] = receipt_entry["payment"]["amount"] - receipt_entry["total"] if receipt_entry["payment"]["type"] == "cash" else 0
+        receipt_entry["created_at"] = datetime.fromtimestamp(receipt_entry["created_at"]).strftime("%d.%m.%Y %H:%M")
+        products = []
+        for product in receipt_entry["products"]:
+            if isinstance(product, Product):
+                product_dict = product.model_dump()
+                product_dict["total"] = float(product.price) * float(product.quantity)
+            else:
+                product["total"] = float(product["price"]) * float(product["quantity"])
+                product_dict = product
+            products.append(product_dict)
+        receipt_entry["products"] = products
+        receipt_entry["url"] = f'http://127.0.0.1:8000/bill/{receipt_entry["id"]}'
+        return cls(**receipt_entry)
 
 
 class User:
     @staticmethod
-    async def create(username, password, full_name):
-        hashed_password = pwd_context.hash(password)
+    async def create(user: UserCreate):
+        hashed_password = pwd_context.hash(user.password)
         async with aiosqlite.connect(DB_PATH) as db:
             await db.execute("INSERT INTO users (username, password, full_name) VALUES (?, ?, ?)",
-                             (username, hashed_password, full_name))
+                             (user.username, hashed_password, user.full_name))
             await db.commit()
 
     @staticmethod
-    async def authenticate(username, password):
+    async def authenticate(username: str, password: str):
         async with aiosqlite.connect(DB_PATH) as db:
             async with db.execute("SELECT username, password FROM users WHERE username = ?", (username,)) as cursor:
                 user = await cursor.fetchone()
@@ -53,20 +139,15 @@ class User:
         return False
 
     @staticmethod
-    async def get_user_by_id(user_id):
-        async with aiosqlite.connect(DB_PATH) as db:
-            async with db.execute("SELECT * FROM users WHERE id = ?", (user_id,)) as cursor:
-                return await cursor.fetchone()
-
-    @staticmethod
     async def get_user_id(username: str):
         async with aiosqlite.connect(DB_PATH) as db:
             async with db.execute("SELECT id FROM users WHERE username = ?", (username,)) as cursor:
-                row = await cursor.fetchone()
-                return row[0]
+                return await cursor.fetchone()
 
+
+class Token:
     @staticmethod
-    async def save_tokens(user_id: int, access_token: str, refresh_token: str):
+    async def save_tokens(user_id: tuple, access_token: str, refresh_token: str):
         access_expires_at = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         refresh_expires_at = datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
 
@@ -74,11 +155,11 @@ class User:
             await db.execute('''
                 INSERT INTO tokens (user_id, access_token, refresh_token, access_expires_at, refresh_expires_at) 
                 VALUES (?, ?, ?, ?, ?)
-            ''', (user_id, access_token, refresh_token, access_expires_at.isoformat(), refresh_expires_at.isoformat()))
+            ''', (user_id[0], access_token, refresh_token, access_expires_at.isoformat(), refresh_expires_at.isoformat()))
             await db.commit()
 
     @staticmethod
-    async def update_access_token(user_id: int, new_access_token: str):
+    async def update_access_token(user_id: tuple, new_access_token: str):
         new_access_expires_at = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
         async with aiosqlite.connect(DB_PATH) as db:
@@ -86,7 +167,7 @@ class User:
                 UPDATE tokens 
                 SET access_token = ?, access_expires_at = ?
                 WHERE user_id = ?
-            ''', (new_access_token, new_access_expires_at.isoformat(), user_id))
+            ''', (new_access_token, new_access_expires_at.isoformat(), user_id[0]))
             await db.commit()
 
     @staticmethod
@@ -110,12 +191,173 @@ class User:
         try:
             payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
             if payload['exp'] < datetime.now(timezone.utc).timestamp():
-                raise jwt.ExpiredSignatureError("Refresh token has expired")
-            username = payload.get("sub")
-            if not username:
-                raise jwt.InvalidTokenError("Invalid token")
-            return username
-        except jwt.ExpiredSignatureError as e:
-            raise e
-        except jwt.InvalidTokenError as e:
-            raise e
+                raise ExpiredSignatureError("Refresh token has expired")
+            return payload.get("sub")
+        except ExpiredSignatureError as e:
+            raise HTTPException(status_code=401, detail="Refresh token has expired")
+        except JWTError as e:
+            raise HTTPException(status_code=401, detail="Invalid token signature")
+
+    @staticmethod
+    async def verify_access_token(token: str) -> str:
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            return payload.get("sub")
+        except JWTError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+    @staticmethod
+    async def get_current_user(token: str = Depends(oauth2_scheme)) -> int:
+        username = await Token.verify_access_token(token)
+        if not username:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token credentials")
+
+        user_id = await User.get_user_id(username)
+        if not user_id:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+        return user_id
+
+
+class Bill:
+    @staticmethod
+    async def save_bill(bill_data: dict):
+        async with aiosqlite.connect(DB_PATH) as db:
+            await db.execute('''
+                INSERT INTO bills (id, user_id, created_at, total, payment_type, payment_amount, notes)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            ''', (bill_data['id'],
+                  bill_data['user_id'][0],
+                  bill_data['created_at'],
+                  float(bill_data['total']),
+                  bill_data['payment']['type'],
+                  float(bill_data['payment']['amount']),
+                  bill_data.get('notes')))
+
+            for product in bill_data['products']:
+                await db.execute('''
+                    INSERT INTO bill_products (bill_id, name, price, quantity)
+                    VALUES (?, ?, ?, ?)
+                ''', (bill_data['id'],
+                      product.name,
+                      float(product.price),
+                      float(product.quantity)))
+
+            await db.commit()
+
+    @staticmethod
+    async def get_filtered_bills(
+        user_id: int,
+        date_from: Optional[str] = None,
+        date_to: Optional[str] = None,
+        min_total: Optional[float] = None,
+        max_total: Optional[float] = None,
+        payment_type: Optional[str] = None,
+        limit: int = 10,
+        offset: int = 0
+    ) -> List[BillResponse]:
+
+        query = "SELECT * FROM bills WHERE user_id = ?"
+        params = [user_id[0]]
+
+        if date_from:
+            date_from_timestamp = datetime.strptime(date_from, "%Y-%m-%d").timestamp()
+            query += " AND created_at >= ?"
+            params.append(date_from_timestamp)
+        if date_to:
+            date_to_timestamp = datetime.strptime(date_to, "%Y-%m-%d").timestamp()
+            query += " AND created_at <= ?"
+            params.append(date_to_timestamp)
+        if min_total is not None:
+            query += " AND total >= ?"
+            params.append(min_total)
+        if max_total is not None:
+            query += " AND total <= ?"
+            params.append(max_total)
+        if payment_type:
+            query += " AND payment_type = ?"
+            params.append(payment_type)
+
+        query += " LIMIT ? OFFSET ?"
+        params.extend([limit, offset])
+
+        async with aiosqlite.connect(DB_PATH) as db:
+            cursor = await db.execute(query, params)
+            rows = await cursor.fetchall()
+            bills = []
+            for row in rows:
+                bill = {
+                    "id": row[0],
+                    "user_id": row[1],
+                    "created_at": row[2],
+                    "total": row[3],
+                    "payment": {"type": row[4], "amount": row[5]},
+                    "notes": row[6],
+                    "products": []
+                }
+
+                product_cursor = await db.execute("SELECT name, price, quantity FROM bill_products WHERE bill_id = ?", (bill["id"],))
+                products = await product_cursor.fetchall()
+                for product in products:
+                    bill["products"].append({
+                        "name": product[0],
+                        "price": product[1],
+                        "quantity": product[2],
+                    })
+
+                bills.append(BillResponse.from_receipt(bill))
+
+            return bills
+
+    @staticmethod
+    async def get_bill_by_id(bill_id: str) -> Optional[BillResponse]:
+        async with aiosqlite.connect(DB_PATH) as db:
+            cursor = await db.execute("SELECT * FROM bills WHERE id = ?", (bill_id,))
+            row = await cursor.fetchone()
+
+            if not row:
+                return None
+            receipt = {
+                "id": row[0],
+                "user_id": row[1],
+                "created_at": row[2],
+                "total": row[3],
+                "payment": {"type": row[4], "amount": row[5]},
+                "notes": row[6],
+                "products": []
+            }
+
+            product_cursor = await db.execute("SELECT name, price, quantity FROM bill_products WHERE bill_id = ?", (bill_id,))
+            products = await product_cursor.fetchall()
+            for product in products:
+                receipt["products"].append({
+                    "name": product[0],
+                    "price": product[1],
+                    "quantity": product[2],
+                    "total": product[1] * product[2]
+                })
+
+            return BillResponse.from_receipt(receipt)
+
+    @staticmethod
+    async def get_user_by_bill_id(bill_id: str) -> Dict[str, str]:
+        """
+        Отримує інформацію про користувача (username та full_name) на основі bill_id
+        """
+        async with aiosqlite.connect(DB_PATH) as db:
+            # Спочатку отримуємо user_id з таблиці bills
+            cursor = await db.execute("SELECT user_id FROM bills WHERE id = ?", (bill_id,))
+            bill_row = await cursor.fetchone()
+            
+            if not bill_row:
+                raise HTTPException(status_code=404, detail="Bill not found")
+
+            user_id = bill_row[0]
+
+            # Тепер отримуємо username та full_name з таблиці users
+            user_cursor = await db.execute("SELECT username, full_name FROM users WHERE id = ?", (user_id,))
+            user_info = await user_cursor.fetchone()
+            
+            if not user_info:
+                raise HTTPException(status_code=404, detail="User not found")
+
+            return {"username": user_info[0], "full_name": user_info[1]}
